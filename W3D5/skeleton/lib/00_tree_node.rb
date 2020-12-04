5020c1856300b5c96efba6489c7b31a059adf8aa
Lamar Poole <keithpoole123@gmail.com>
2020-12-04 18:05:42 -0500

    fixed parent=,added add and remove child methods

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -7,33 +7,26 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(node)
+    
         
-        if node != nil
-            @parent = node
-            class PolyTreeNode
-                attr_accessor :value, :parent, :children
-            
-                def initialize(value)
-                    @value = value
-                    @parent = nil
-                    @children = []
-                end
-            
-                def parent=(node)
-                    if node != nil
-                        @parent = node
-                        @children.shift
-                        node.children << self unless node.children.include?(self)
-                    else
-                        @parent = node
-                    end
-                end
+    def parent=(node) 
+             @parent.children.delete(self) if @parent != nil
+            if node != nil
+                @parent = node
+                
+                node.children << self unless node.children.include?(self)
+            else
+                @parent = node
             end
-            node.children << self unless node.children.include?(self)
-        else
-            @parent = node
-        end
     end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        child_node.parent = nil
+    end
+
 end
 
