9ffee4e6fe6f4ce97f780f900512ddadf38b40b3
timjkim210 <timjkim210@gmail.com>
2020-12-09 15:09:41 -0800

    finished pawn

--- W4D2/chess/pawn.rb
+++ W4D2/chess/pawn.rb
@@ -4,7 +4,7 @@ class Pawn < Piece
     end
     
     def moves
-
+        forward_steps + side_attacks
     end
 
     def at_start_row?
@@ -23,4 +23,37 @@ class Pawn < Piece
             return -1
         end
     end
+
+    def forward_steps
+        x, y = pos
+        all_moves = []
+        one_move = [x + forward_dir, y]
+        two_move = [x + forward_dir + forward_dir, y]
+
+        if @board[one_move].empty?
+            all_moves << one_move
+        end
+
+        if @board[two_move].empty? && @board[one_move].empty?
+            all_moves << two_move
+        end
+        all_moves
+    end
+
+    def side_attacks
+        x, y = pos
+        all_moves = []
+        left = [x + forward_dir, y-1]
+        right = [x + forward_dir, y+1]
+        all_moves = [left, right]
+        attacks = []
+        all_moves.each do |move|
+            if !@board[move].empty? && (@board[move].color != self.color)
+                attacks << move
+            end
+        end
+
+        attacks
+    end
+
 end
