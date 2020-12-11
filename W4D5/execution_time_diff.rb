ffa78004ff4f3097d9116f21b592ed484e8732c7
James Koenig <Koenig.James@Live.com>
2020-12-11 11:14:52 -0800

    almost done with this project! :)
    committing to switch roles

--- W4D5/execution_time_diff.rb
+++ W4D5/execution_time_diff.rb
@@ -1,1 +1,32 @@
-Hello
+
+module Phase1
+  def my_min(list)
+    list.each do |ele1|
+      return ele1 if list.all? { |ele2| ele1 < ele2 }
+    end
+  end
+
+  def largest_contiguous_subsum(list)
+  end
+
+  def subarrays(array)
+    (0...array.length).each do 
+    end
+  end
+end
+
+module Phase2
+  def my_min(list)
+    smallest = nil
+    list.each do |ele1|
+      if smallest == nil || ele1 < smallest
+        smallest = ele1
+      end
+    end
+    return smallest
+  end
+
+  def largest_contiguous_subsum(list)
+  end
+end
+
