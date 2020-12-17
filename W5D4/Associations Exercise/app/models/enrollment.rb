c7634c13ce9d46d9767c3812b057ee0fb54d9fde
timjkim210 <timjkim210@gmail.com>
2020-12-17 14:30:46 -0800

    enrollment model

--- W5D4/Associations Exercise/app/models/enrollment.rb
+++ W5D4/Associations Exercise/app/models/enrollment.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# courses have many users, and users have many courses
+# enrollment can belong to both user and course
+
 class Enrollment < ApplicationRecord
+    belongs_to :user,
+        foreign_key: :student_id,
+        primary_key: :id,
+        class_name: :User
+
+    belongs_to :course,
+        foreign_key: :course_id,
+        primary_key: :id,
+        class_name: :Course
 end
+
