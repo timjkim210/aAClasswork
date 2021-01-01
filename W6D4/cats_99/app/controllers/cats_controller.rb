3a65a22cb123ad6ca1e1971f06661bf632513046
timjkim210 <timjkim210@gmail.com>
2020-12-31 16:44:34 -0800

    finish new and create views

--- W6D4/cats_99/app/controllers/cats_controller.rb
+++ W6D4/cats_99/app/controllers/cats_controller.rb
@@ -5,12 +5,28 @@ class CatsController < ApplicationController
     end
 
     def show
-        @cat = Cat.find_by(params[:id])
+        @cat = Cat.find(params[:id])
         render :show
     end
 
     def new
         @cat = Cat.new
         render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def cat_params
+        params.require(:cat).permit(:age, :birth_date, :color, :name, :sex, :description)
     end
 end
