a61e6f1dfdd7851670746f61b2f88ca9bdebba86
appkenny <kzeng.95@gmail.com>
2021-01-06 14:07:17 -0800

    set up file and began user specs

---
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6)}
+  it { should have_many(:goals) }
+
+  describe "session token assign to a user" do
+    
+  end
+end
