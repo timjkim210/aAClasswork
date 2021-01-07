cac31025beafddcaf81476e3ee3e1c15fb087707
appkenny <kzeng.95@gmail.com>
2021-01-06 16:37:57 -0800

    continued on user controller action specs

--- W7D3/goal_app/spec/controllers/users_controller_spec.rb
+++ W7D3/goal_app/spec/controllers/users_controller_spec.rb
@@ -10,4 +10,41 @@ RSpec.describe UsersController, type: :controller do
         end
     end
 
+    describe "GET #new" do
+      it "renders new users page" do
+        get :new
+        expect(response).to be_success
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe "GET #show" do
+      it "renders a user" do
+        User.create!(name: "joe", email: "joe@email.com", password: "123456")
+        get :show, id: 1
+        expect(response).to be_success
+        expect(response).to render_template(:show)
+      end
+    end
+
+    describe "GET #edit" do
+      it "renders a edit users page" do
+        get :edit
+        expect(response).to be_success
+        expect(response).to render_template(:edit)
+      end 
+    end
+
+    describe "POST #create" do
+      context "with invalid params" do
+        it "renders the new template" do
+
+        end
+      end
+
+      context "with valid params" do
+
+      end
+    end
+
 end
