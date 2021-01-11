38f997a701fbf7886300eaebf91571edf8b06c92
droid4alex <droid4alex@gmail.com>
2021-01-11 10:04:15 -0800

    updated controllers

--- W8D1/reddit_clone/app/controllers/application_controller.rb
+++ W8D1/reddit_clone/app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  #crlll
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user = User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
+    #redirect_to new_session_url  #can we do this? 
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
---
+++ W8D1/reddit_clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to user_url(@user)
+      #redirect_to        #asked if/when redirect is needed. Zach said redirect_to is a whole new request   do not render :new here after logging in a user (because they are already logged in)
+    else
+      flash.now[:errors] = ['Invalid username or password']
+      render :new         #render :new allows the previously entered data to still be there when the page is refreshed for them to try again
+    end
+  end
+
+  def destroy
+    
+  end
+end
