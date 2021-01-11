ec27787578c1f28ec24093905d9fbf70caebc18e
droid4alex <droid4alex@gmail.com>
2021-01-11 14:21:50 -0800

    Worked on posts controller

--- W8D1/reddit_clone/app/controllers/posts_controller.rb
+++ W8D1/reddit_clone/app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
+    before_action :require_logged_in
+
     def show
-        @sub = Sub.find_by(params[:sub_id])
+        @post = Post.find(params[:id])
         render :show
     end
+
+    def new
+        @post = Post.new
+        render :new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.author_id = current_user.id
+        if
+        else
+        end
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :url, :content, :sub_id)
+    end
 end
+
