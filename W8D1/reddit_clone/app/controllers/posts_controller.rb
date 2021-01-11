b2ba408df4ba4043cec65fcdb96b52b48ed9f2c7
timjkim210 <timjkim210@gmail.com>
2021-01-11 14:49:19 -0800

    started working on user views

--- W8D1/reddit_clone/app/controllers/posts_controller.rb
+++ W8D1/reddit_clone/app/controllers/posts_controller.rb
@@ -14,8 +14,26 @@ class PostsController < ApplicationController
     def create
         @post = Post.new(post_params)
         @post.author_id = current_user.id
-        if
+        if @post.save
+            redirect_to post_url(@post)
         else
+            flash.now[:errors] = @post.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @post = current_user.authored_posts.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_url(@post)
+        else
+            flash.now[:errors] = @post.errors.full_messages
+            render :edit
         end
     end
 
