af3df6a03064b0aeaf8e9cae4d7ad7c444af244e
timjkim210 <timjkim210@gmail.com>
2021-01-19 14:05:24 -0800

    sum

--- W9D1/arguments_exercises.js
+++ W9D1/arguments_exercises.js
@@ -1,1 +1,18 @@
-hi
+// function add() {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+function add(...args) {
+    return args.reduce((acc, el) => {
+        return acc + el;
+    })
+}
+
+console.log(add(1, 2, 3, 4, 5))
+
+
+
