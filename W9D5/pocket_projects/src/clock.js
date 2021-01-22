f2698be3cbba0169729690c1d81236a454ded478
timjkim210 <timjkim210@gmail.com>
2021-01-22 14:32:09 -0800

    phase 1

--- W9D5/pocket_projects/src/clock.js
+++ W9D5/pocket_projects/src/clock.js
@@ -7,9 +7,39 @@ class Clock {
         this.minutes = time.getMinutes();
         this.seconds = time.getSeconds();
     }
-    htmlGenerator(this.printTime(), clockElement)
+    htmlGenerator(this.printTime(), document.getElementById('clock'))
     printTime() {
         const timeJoin = [this.hours, this.minutes, this.seconds].join(":");
+        return timeJoin;
     }
 
+
+    _tick() {
+        this._incrementSeconds();
+        htmlGenerator()
+    }
+
+    _incrementSeconds() {
+        this.seconds += 1;
+        if (this.seconds === 60) {
+            this.seconds = 0;
+            this._incrementMinutes();
+        }
+    }
+
+    _incrementMinutes() {
+        this.minutes += 1;
+        if (this.minutes === 60) {
+            this.minutes = 0;
+            this._incrementHours();
+        }
+    }
+
+    _incrementHours() {
+        this.hours = (this.hours + 1) % 24;
+    }
+
+
 }
+
+const clock = new Clock();
