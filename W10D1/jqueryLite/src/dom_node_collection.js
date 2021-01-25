27c454c7348a6f18b449dbdf12d72f16ff8ed534
Bogey <jeffreysbogart@gmail.com>
2021-01-25 14:01:18 -0800

    Add append method to DOM class

--- W10D1/jqueryLite/src/dom_node_collection.js
+++ W10D1/jqueryLite/src/dom_node_collection.js
@@ -17,8 +17,22 @@ class DOMNodeCollection {
         });
     }
 
-    append() {
-        
+    append(arg) {
+        if (arg instanceof HTMLElement){
+            this.elements.forEach(el => {
+                el.innerHTML = arg.outerHTML;
+            });
+        } else if (arg instanceof String) {
+            this.elements.forEach(el => {
+                el.innerHTML = arg;
+            });
+        } else if (arg instanceof DOMNodeCollection) {
+            arg.elements.forEach(el => {
+                this.elements.forEach(el2 => {
+                    el2.innerHTML = el.outerHTML;
+                });
+            });
+        }
     }
 }
 module.exports = DOMNodeCollection
