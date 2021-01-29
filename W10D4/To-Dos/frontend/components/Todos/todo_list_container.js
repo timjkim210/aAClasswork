62503760448f9369a6166affe785bc15099b5e43
timjkim210 <timjkim210@gmail.com>
2021-01-29 14:20:43 -0800

    up to todo list item

---
+++ W10D4/To-Dos/frontend/components/Todos/todo_list_container.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux'
+import TodoList from './todoList'
+import {allTodos} from '../../reducers/selectors'
+import {receiveTodo, receiveTodos, removeTodo} from '../../actions/todo_actions'
+
+function mapStateToProps(state) {
+    const todos = allTodos(state);
+    return {todos};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        receiveTodo: (todo) => dispatch(receiveTodo(todo)),
+        receiveTodos: (todos) => dispatch(receiveTodos(todos)),
+        removeTodo: (id) => dispatch(removeTodo(id))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- W10D4/To-Dos/frontend/components/app.jsx
+++ W10D4/To-Dos/frontend/components/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoListContainer from './Todos/todo_list_container'
 
 export default class App extends React.Component {
     render() {
         return(
-            <h1>To-Dos</h1>
+            <>
+                <h1>To-Dos</h1>
+                <TodoListContainer />
+            </>
         )
     }
 } 
