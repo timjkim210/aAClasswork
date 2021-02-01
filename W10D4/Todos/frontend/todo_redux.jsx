66324f0571e94b402aec1dc7cc9014e8a9f5e8ed
timjkim210 <timjkim210@gmail.com>
2021-02-01 14:42:03 -0800

    started api

---
+++ W10D4/Todos/frontend/todo_redux.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import configureStore from './store/store'
+import Root from './components/root'
+import {receiveTodo, receiveTodos, removeTodo} from './actions/todo_actions'
+import {receiveSteps, receiveStep, removeStep} from './actions/step_actions'
+import {allTodos} from './reducers/selectors'
+import {fetchTodos} from './util/todo_api_util'
+
+document.addEventListener("DOMContentLoaded", function() {
+    const store = configureStore()
+    let reactRoot = document.querySelector("#root")
+    ReactDOM.render(<Root store={store} />, reactRoot)
+    window.store = store;
+    window.receiveTodo = receiveTodo
+    window.receiveTodos = receiveTodos
+    window.removeTodo = removeTodo;
+    window.receiveSteps = receiveSteps;
+    window.receiveStep = receiveStep;
+    window.removeStep = removeStep;
+    window.allTodos = allTodos;
+    window.fetchTodos = fetchTodos;
+})
---
+++ W10D4/Todos/frontend/util/todo_api_util.js
@@ -0,0 +1,10 @@
+import { $CombinedState } from "redux"
+
+export const fetchTodos = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/todos"
+    })
+
+    
+}
