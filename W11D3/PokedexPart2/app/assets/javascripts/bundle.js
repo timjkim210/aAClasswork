9f8ccc6c482667d69e38e5a8da310e18db1893a4
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-02-03 14:23:55 -0800

    Made pokemon clickable and update hash url
    Does not update page or state yet

--- W11D3/PokedexPart2/app/assets/javascripts/bundle.js
+++ W11D3/PokedexPart2/app/assets/javascripts/bundle.js
@@ -268,6 +268,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return PokemonIndexItem; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -292,6 +293,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 
 
+
 var PokemonIndexItem = /*#__PURE__*/function (_React$Component) {
   _inherits(PokemonIndexItem, _React$Component);
 
@@ -309,9 +311,11 @@ var PokemonIndexItem = /*#__PURE__*/function (_React$Component) {
       var pokemon = this.props.pokemon;
       return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
         className: "pokemon-index-item"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+        to: "/pokemon/".concat(pokemon.id)
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, pokemon.id), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
         src: pokemon.imageUrl
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, pokemon.name));
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, pokemon.name)));
     }
   }]);
 
--- W11D3/PokedexPart2/frontend/components/pokemon/pokemon_index_item.jsx
+++ W11D3/PokedexPart2/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {Link } from 'react-router-dom';
 
 export default class PokemonIndexItem extends React.Component {
     render() {
         const pokemon = this.props.pokemon
         return (
             <li className="pokemon-index-item">
-                <span>{pokemon.id}</span>
-                <img src={pokemon.imageUrl} />
-                <span>{pokemon.name}</span>
+                <Link to={`/pokemon/${pokemon.id}`}>
+                    <span>{pokemon.id}</span>
+                    <img src={pokemon.imageUrl} />
+                    <span>{pokemon.name}</span>
+                </Link>
             </li>
         )  
     }
